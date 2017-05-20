e61b487ed879bcd0d0acd6c02d09ada2c64bdca2
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-05-19 18:26:15 -0700

    doc changes

--- src/main/scala/Corplet/chunks.scala
+++ src/main/scala/Corplet/chunks.scala
@@ -3,7 +3,7 @@ package Corplet
 import java.nio.{MappedByteBuffer, ByteBuffer, LongBuffer}
 
 /** Class to validate or read header of .corp file
-  *
+  * @constructor Takes one bytebuffer as an argument
   */
 class HeaderChunk(buf:ByteBuffer) {
 	/** Checks the byte sequence of file header
@@ -17,6 +17,7 @@ class HeaderChunk(buf:ByteBuffer) {
 }
 /** Class to serve as a binary trie nod that travereses the Corplet
   * @note A Gate is a byte slot that can have the values 0, 1 or 2
+  * @note Uses memory mapped buffer for performance and boost and thread safety
   */
 class BodyChunk(buf:MappedByteBuffer, val ref:Corp){
 	buf.position(27)
@@ -25,19 +26,29 @@ class BodyChunk(buf:MappedByteBuffer, val ref:Corp){
 	//sets limit so that index is never read as gate
 	buf.limit(27)
 
+	/** Gets the index/pointer at some position in the index portion of the chunk
+	  * @note indexes are all identical to Java's Long type
+	  *
+	  */
 	def getIndex(key:Char):Long = {
 		indexes.get(Util.inWord(key))
 	}
-
+	/** Sets some index in the chunk for a pointer to some position in the file
+	  * @param ind A 64 bit number that corresponds to some position in the file
+	  */
 	def setIndex(key:Char, ind:Long):Unit = {
 		indexes.put(Util.inWord(key), ind)
 		buf.force()
 	}
-
+	/** Gets the next bodychunk that is present at some index in the file
+	  *
+	  */
 	def getChunkAtIndex(key:Char):BodyChunk = {
 		new BodyChunk(ref.getChunk(getIndex(key)), ref)
 	}
-
+	/** Makes a new chunk at the end of the file and sets it in the specified index
+	  * @note used for inserting new phrases into the corplet.
+	  */
 	def makeNewChunkAtIndex(key:Char):BodyChunk = {
 		val pair:(Long, MappedByteBuffer) = ref.getAndAppendBodyChunk()
 		setIndex(key, pair._1)
